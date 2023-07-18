a44d273112e27ac64c3b24e62721e036c39c8942
Frank220599 <mr.frank220599@gmail.com>
2023-07-19 01:30:47 +0500

    fix

--- src/components/Check/Check.tsx
+++ src/components/Check/Check.tsx
@@ -1,86 +1,87 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import {renderToStaticMarkup} from "react-dom/server";
 
 const Check = () => {
   const { userList } = useSelector((state: RootState) => state.profileSlice);
 
   const { getOrderList } = useSelector((state: RootState) => state.orderSlice);
 
-  return (
+  return renderToStaticMarkup(
       <table id={"check"} className="check-box">
-        <div className="box-top">
-          <div>
-            <div>Номер заказа</div>
-            <div id="id">{getOrderList.id}</div>
+          <div className="box-top">
+              <div>
+                  <div>Номер заказа</div>
+                  <div id="id">{getOrderList.id}</div>
+              </div>
+              <div>
+                  <div>дата время</div>
+                  <div id="id">{getOrderList.created_at}</div>
+                  <div id="id">{getOrderList.created_time}</div>
+              </div>
           </div>
-          <div>
-            <div>дата время</div>
-            <div id="id">{getOrderList.created_at}</div>
-            <div id="id">{getOrderList.created_time}</div>
+          <div className="username">{getOrderList.user?.full_name}</div>
+          <div className="table-id">
+              ID <strong>{getOrderList.user?.id}</strong>
           </div>
-        </div>
-        <div className="username">{getOrderList.user?.full_name}</div>
-        <div className="table-id">
-          ID <strong>{getOrderList.user?.id}</strong>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th className="content">№</th>
-              <th>Наименование товара</th>
-              <th className="rotate content">ед.изм</th>
-              <th className="rotate content">кол-во</th>
-              <th>Цена</th>
-              <th>Сумма</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getOrderList.orderItems?.map((item) => {
-              return (
-                <tr>
-                  <td>{item.id}</td>
-                  <td>{item.product.name}</td>
-                  <td>шт</td>
-                  <td>{item.count}</td>
-                  <td>{item.price}</td>
-                  <td>{item.total_price}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div className="total">
-          <div className="check-total">ИТОГО </div>
-          {/* <div className="check-amount">12</div> */}
-          <div className="check-price">
-            {getOrderList.price?.toLocaleString("ru-RU")}
+          <table>
+              <thead>
+              <tr>
+                  <th className="content">№</th>
+                  <th>Наименование товара</th>
+                  <th className="rotate content">ед.изм</th>
+                  <th className="rotate content">кол-во</th>
+                  <th>Цена</th>
+                  <th>Сумма</th>
+              </tr>
+              </thead>
+              <tbody>
+              {getOrderList.orderItems?.map((item) => {
+                  return (
+                      <tr>
+                          <td>{item.id}</td>
+                          <td>{item.product.name}</td>
+                          <td>шт</td>
+                          <td>{item.count}</td>
+                          <td>{item.price}</td>
+                          <td>{item.total_price}</td>
+                      </tr>
+                  );
+              })}
+              </tbody>
+          </table>
+          <div className="total">
+              <div className="check-total">ИТОГО </div>
+              {/* <div className="check-amount">12</div> */}
+              <div className="check-price">
+                  {getOrderList.price?.toLocaleString("ru-RU")}
+              </div>
           </div>
-        </div>
-        {/* <div className="block">
+          {/* <div className="block">
             Лимит
             <div className="block-right">
               {userList.limit_summa?.toLocaleString("ru-RU")}
             </div>
           </div> */}
-        {/* <div className="block">
+          {/* <div className="block">
             Итого
             <div className="block-right">
               {getOrderList.price?.toLocaleString("ru-RU")}
             </div>
           </div> */}
-        <div className="block">
-          Остаток денежные средств
-          <div className="block-right">
-            {userList.balance?.toLocaleString("ru-RU")}
+          <div className="block">
+              Остаток денежные средств
+              <div className="block-right">
+                  {userList.balance?.toLocaleString("ru-RU")}
+              </div>
           </div>
-        </div>
-        <div className="block">
-          Остаток лимита
-          <div className="block-right">
-            {userList.limit_summa?.toLocaleString("ru-RU")}
+          <div className="block">
+              Остаток лимита
+              <div className="block-right">
+                  {userList.limit_summa?.toLocaleString("ru-RU")}
+              </div>
           </div>
-        </div>
       </table>
   );
 };
--- src/components/ConfirmOrder/confirmOrder.tsx
+++ src/components/ConfirmOrder/confirmOrder.tsx
@@ -7,10 +7,11 @@ import useRootStore from "../../hook/useRootStore";
 import {APP_ROUTES} from "../../router/Route";
 import Title from "../Title/title";
 import styles from "./confirmOrder.module.css";
-import {useDispatch} from "react-redux";
-import {Dispatch} from "../../store";
+import {useDispatch, useSelector} from "react-redux";
+import {Dispatch, RootState} from "../../store";
 import "../Check/Check.css";
 import Check from "../Check/Check";
+import {renderToStaticMarkup, renderToString} from "react-dom/server";
 
 const ConfirmOrder = () => {
     const {visiable, hide} = useRootStore().visiibleStore;
@@ -22,8 +23,8 @@ const ConfirmOrder = () => {
             data: {shop_id: 1},
             callback: () => {
                 handlePrint();
-                localStorage.clear();
-                dispatch.profileSlice.logout();
+                // localStorage.clear();
+                // dispatch.profileSlice.logout();
                 navigation(APP_ROUTES.MAIN);
             },
         });
@@ -31,17 +32,98 @@ const ConfirmOrder = () => {
         hide("confirmOrder");
     };
 
-    const handlePrint = () => {
+    const {userList} = useSelector((state: RootState) => state.profileSlice);
+
+    const {getOrderList} = useSelector((state: RootState) => state.orderSlice);
+
+    const handlePrint = async () => {
         const ipcRenderer = window.require("electron").ipcRenderer;
 
         function sendCommandToWorker(content: any) {
             ipcRenderer.send("printPDF", content);
         }
 
-        const check = document.getElementById('check');
-        sendCommandToWorker(`${check}`);
-    };
+        console.log({userList,getOrderList})
 
+        const a = renderToStaticMarkup(
+            <table id={"check"} className="check-box">
+                <div className="box-top">
+                    <div>
+                        <div>Номер заказа</div>
+                        <div id="id">{getOrderList.id}</div>
+                    </div>
+                    <div>
+                        <div>дата время</div>
+                        <div id="id">{getOrderList.created_at}</div>
+                        <div id="id">{getOrderList.created_time}</div>
+                    </div>
+                </div>
+                <div className="username">{getOrderList.user?.full_name}</div>
+                <div className="table-id">
+                    ID <strong>{getOrderList.user?.id}</strong>
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th className="content">№</th>
+                        <th>Наименование товара</th>
+                        <th className="rotate content">ед.изм</th>
+                        <th className="rotate content">кол-во</th>
+                        <th>Цена</th>
+                        <th>Сумма</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {getOrderList.orderItems?.map((item) => {
+                        return (
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.product.name}</td>
+                                <td>шт</td>
+                                <td>{item.count}</td>
+                                <td>{item.price}</td>
+                                <td>{item.total_price}</td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
+                <div className="total">
+                    <div className="check-total">ИТОГО</div>
+                    {/* <div className="check-amount">12</div> */}
+                    <div className="check-price">
+                        {getOrderList.price?.toLocaleString("ru-RU")}
+                    </div>
+                </div>
+                {/* <div className="block">
+            Лимит
+            <div className="block-right">
+              {userList.limit_summa?.toLocaleString("ru-RU")}
+            </div>
+          </div> */}
+                {/* <div className="block">
+            Итого
+            <div className="block-right">
+              {getOrderList.price?.toLocaleString("ru-RU")}
+            </div>
+          </div> */}
+                <div className="block">
+                    Остаток денежные средств
+                    <div className="block-right">
+                        {userList.balance?.toLocaleString("ru-RU")}
+                    </div>
+                </div>
+                <div className="block">
+                    Остаток лимита
+                    <div className="block-right">
+                        {userList.limit_summa?.toLocaleString("ru-RU")}
+                    </div>
+                </div>
+            </table>
+        )
+
+        sendCommandToWorker(a);
+    };
 
     const BackOrder = () => {
         hide("confirmOrder");
@@ -77,7 +159,6 @@ const ConfirmOrder = () => {
                             fontSize="28px"
                         />
                     </div>
-                    <Check/>
                 </div>
             </Backdrop>
         </div>
